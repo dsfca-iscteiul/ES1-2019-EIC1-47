2af225d6cfef73e11215493488cc4be968ceadf5
daniela <daniela@192.168.1.3>
2019-11-01 14:41:36 +0000

    Criado repositorio

---
+++ ES1-2019-EIC1-47/src/InterfaceUser.java
@@ -0,0 +1,13 @@
+
+public class InterfaceUser {
+
+	public InterfaceUser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
