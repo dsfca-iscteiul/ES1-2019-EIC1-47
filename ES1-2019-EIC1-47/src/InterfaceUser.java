46c6643c6eb05f703d0ddf5f9484f8c7633cf4f2
Catarina Teodoro <cssto@iscte-iul.pt>
2019-11-09 22:23:15 +0000

    Componentes do menu criados

--- ES1-2019-EIC1-47/src/InterfaceUser.java
+++ ES1-2019-EIC1-47/src/InterfaceUser.java
@@ -1,8 +1,34 @@
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class InterfaceUser {
+	
+	private JFrame frame;
+	private JLabel menu;
+	private Button openFile;
+	private Button showFile;
+	private Button editThresholds;
+	private Button testPlatform;
+	private JPanel center;
 
 	public InterfaceUser() {
-		// TODO Auto-generated constructor stub
+		frame = new JFrame("Menu");
+		frame.setLayout(new BorderLayout());
+		
+		center = new JPanel();
+		center.setLayout(new GridLayout(4,1,3,3));
+		
+		menu = new JLabel("MENU", SwingConstants.CENTER);
+		openFile = new Button("Select file to analize");
+		showFile = new Button("Show file");
+		editThresholds = new Button("Edit thresholds");
+		testPlatform = new Button("Show results");
 	}
 
 	public static void main(String[] args) {
