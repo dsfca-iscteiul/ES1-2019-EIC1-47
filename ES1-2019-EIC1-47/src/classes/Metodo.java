08143d4ea050690193cb2da681467a368739eb6f
jptmo1 <jptmo1@iscte-iul.pt>
2019-12-12 21:56:08 +0000

    JavaDoc da classe da Classe Metodo,RegrasDetencao

--- ES1-2019-EIC1-47/src/classes/Metodo.java
+++ ES1-2019-EIC1-47/src/classes/Metodo.java
@@ -20,144 +20,292 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 	
+/**
+ * Date: Dec 12, 2019
+ * @author engin
+ *
+ */
 public class Metodo {
-	
+	/**
+	 * methodID-coluna 1 do file
+	 */
 	private double methodID;
+	/**
+	 * package1-coluna 2 do file
+	 */
 	private String package1;
+	/**
+	 * class1-coluna 3 do file
+	 */
 	private String class1;
+	/**
+	 * method-coluna 4 do file
+	 */
 	private String method;
+	/**
+	 * loc-coluna 5 do file
+	 */
 	private double loc;
+	/**
+	 * cyclo-coluna 6 do file
+	 */
 	private double cyclo;
+	/**
+	 * atfd-coluna 7 do file
+	 */
 	private double atfd;
+	/**
+	 * atfd-coluna 8 do file
+	 */
 	private double laa;
+	/**
+	 * s_long_method-coluna 9 do file
+	 */
 	private boolean is_long_method;
+	/**
+	 * iPlasma-coluna 10 do file
+	 */
 	private boolean iPlasma;
+	/**
+	 * PMD-coluna 11 do file
+	 */
 	private boolean PMD;
+	/**
+	 *is_feature_envy-coluna 12 do file
+	 */
 	private boolean is_feature_envy;
+	/**
+	 * ruleResult
+	 */
 	private boolean ruleResult;
-
+	
+	/**
+	 * 
+	 * @param loc
+	 */
 	public void setloc(double loc) {
 		this.loc = loc;
 	}
-
+	
+	/**
+	 * 
+	 * @return isPlasma
+	 */
 	public boolean isiPlasma() {
 		return iPlasma;
 	}
 
-
+	/**
+	 * 
+	 * @param iPlasma
+	 */
 	public void setiPlasma(boolean iPlasma) {
 		this.iPlasma = iPlasma;
 	}
 
-
+	/**
+	 * 
+	 * @return is_feature_envy
+	 */
 	public boolean isIs_feature_envy() {
 		return is_feature_envy;
 	}
 
-
+	/**
+	 * 
+	 * @param is_feature_envy
+	 */
 	public void setIs_feature_envy(boolean is_feature_envy) {
 		this.is_feature_envy = is_feature_envy;
 	}
 
-
-		@Override
+	/**
+	 *  The toString() method was changed for better readability, and it returns the given name of the rule, followed by the preset
+	 */
 	public String toString() {
 		return "Metodo [methodID=" + methodID + ", package1=" + package1 + ", class1=" + class1 + ", method=" + method
 				+ ", loc="+ loc + ", cyclo" + cyclo+  ", atf"+ atfd+  "]";
 	}
 
-	
+	/**
+	 * 
+	 */
 	public Metodo() {
 		super();
 		
 	}
-
+	/**
+	 * 
+	 * @return methodID
+	 */
 	public double getMethodID() {
 		return methodID;
 	}
-
+	
+	/**
+	 * 
+	 * @param d
+	 */
 	public void setMethodID(double d) {
 		this.methodID = d;
 	}
-
+	
+	/**
+	 * 
+	 * @return methodID
+	 */
 	public String getPackage1() {
 		return package1;
 	}
 	
-	
-
+	/**
+	 * 
+	 * @param package1
+	 */
 	public void setPackage1(String package1) {
 		this.package1 = package1;
 	}
-
+	
+	/**
+	 *  
+	 * @return class1
+	 */
 	public String getClass1() {
 		return class1;
 	}
-
+	
+	/**
+	 * 
+	 * @param class1
+	 */
 	public void setClass1(String class1) {
 		this.class1 = class1;
 	}
-
+	
+	/**
+	 * 
+	 * @return method
+	 */
 	public String getMethod() {
 		return method;
 	}
-
+	
+	/**
+	 * 
+	 * @param method
+	 */
 	public void setMethod(String method) {
 		this.method = method;
 	}
-
+	
+	/**
+	 * 
+	 * @return loc
+	 */
 	public double getLoc() {
 		return loc;
 	}
-
+	
+	/**
+	 * 
+	 * @param loc
+	 */
 	public void setLoc(double loc) {
 		this.loc = loc;
 	}
-
+	
+	/**
+	 * 
+	 * @return cyclo
+	 */
 	public double getCyclo() {
 		return cyclo;
 	}
-
+	
+	/**
+	 * 
+	 * @param cyclo
+	 */
 	public void setCyclo(double cyclo) {
 		this.cyclo = cyclo;
 	}
-
+	
+	/**
+	 * 
+	 * @return atfd
+	 */
 	public double getAtfd() {
 		return atfd;
 	}
-
+	
+	/**
+	 * 
+	 * @param atfd
+	 */
 	public void setAtfd(double atfd) {
 		this.atfd = atfd;
 	}
-
+	
+	/**
+	 * 
+	 * @return laa
+	 */
 	public double getLaa() {
 		return laa;
 	}
-
+	
+	/**
+	 * 
+	 * @param laa
+	 */
 	public void setLaa(double laa) {
 		this.laa = laa;
 	}
-
+	
+	/**
+	 * 
+	 * @return is_long_method
+	 */
 	public boolean isIs_long_method() {
 		return is_long_method;
 	}
-
+	
+	/**
+	 * 
+	 * @param is_long_method
+	 */
 	public void setIs_long_method(boolean is_long_method) {
 		this.is_long_method = is_long_method;
 	}
-
+	
+	/**
+	 *
+	 * @return PMD
+	 */
 	public boolean isPMD() {
 		return PMD;
 	}
-
+	
+	/**
+	 *
+	 * @param pMD
+	 */
 	public void setPMD(boolean pMD) {
 		PMD = pMD;
 	}
 	
+	/**
+	 * 
+	 * @return ruleResult
+	 */
 	public boolean getRuleResult() {
 		return ruleResult;
 	}
-
+	
+	/**
+	 * 
+	 * @param ruleResult
+	 */
 	public void setRuleResult(boolean ruleResult) {
 		this.ruleResult = ruleResult;
 	}
--- ES1-2019-EIC1-47/src/classes/RegrasDetencao.java
+++ ES1-2019-EIC1-47/src/classes/RegrasDetencao.java
@@ -1,11 +1,24 @@
 package classes;
 import java.util.ArrayList;
 
+/**
+ * Date: Dec 12, 2019
+ * @author engin
+ *
+ */
 public class RegrasDetencao {
+	/**
+	 * lista dos metodos
+	 */
 	private ArrayList<Metodo>lista;
+	/**
+	 * RuleChecker
+	 */
 	private RuleChecker rc = new RuleChecker();
-
-	// Preset do feature envy e do long method
+	
+	/**
+	 * Preset do feature envy e do long method
+	 */
 	public void Verificacao() {
 		//lista com a serializacao dos dados do excel
 		lista = LeituraFicheiro.getDados();
@@ -16,7 +29,15 @@ public class RegrasDetencao {
 			System.out.println("FeatureEnvyCheck = " + rc.featureEnvyCheck((int)m.getAtfd(), (float)m.getLaa()) + " LongMethodCheck = " + rc.longMethodCheck((int)m.getLoc(), (float)m.getCyclo()));
 		}
 	}
-	// verificao do feature envy e long method, com dados postos pelo utilizador
+	
+	/**
+	 * metodo verificao do feature envy e long method, com dados postos pelo utilizador
+	 * @param ATFDLimit
+	 * @param LAALimit
+	 * @param LOCLimit
+	 * @param CYCLOLimit
+	 * @throws Exception
+	 */
 	public void Verificacao(int ATFDLimit, int LAALimit, int LOCLimit, int CYCLOLimit) throws Exception {
 		//lista com a serializacao dos dados do excel
 		lista = LeituraFicheiro.getDados();
@@ -26,7 +47,16 @@ public class RegrasDetencao {
 			rc.longMethodCheck((int)m.getLoc(), (int)m.getCyclo(), LOCLimit, CYCLOLimit);		
 		}
 	}
-	// verificao do feature envy e long method, com dados postos pelo utilizador mais logic parser
+	
+	/**
+	 * metodo verificao do feature envy e long method, com dados postos pelo utilizador mais logic parser
+	 * @param ATFDLimit
+	 * @param LAALimit
+	 * @param LOCLimit
+	 * @param CYCLOLimit
+	 * @param lp
+	 * @throws Exception
+	 */
 	public void Verificacao(int ATFDLimit, int LAALimit, int LOCLimit, int CYCLOLimit, LogicParser lp) throws Exception {
 		//lista com a serializacao dos dados do excel
 		lista = LeituraFicheiro.getDados();
@@ -37,6 +67,11 @@ public class RegrasDetencao {
 										//perguntar este int
 		} 
 	}
+	
+	/**
+	 * metodo para ver o DCI na comparacao iPlasma e PMD com Is_long_method
+	 * 
+	 */
 	public void DCI() {
 		lista = LeituraFicheiro.getDados();
 		for(Metodo m: lista) {
@@ -45,6 +80,10 @@ public class RegrasDetencao {
 
 		}
 	}
+	
+	/**
+	 * metodo para ver o DII na comparacao iPlasma e PMD com Is_long_method
+	 */
 	public void DII() {
 		//lista com a serializacao dos dados do excel
 		lista = LeituraFicheiro.getDados();
@@ -54,6 +93,10 @@ public class RegrasDetencao {
 				System.out.println("DII =  " + m);
 		}
 	}
+	
+	/**
+	 * metodo para ver o ADCI na comparacao iPlasma e PMD com Is_long_method
+	 */
 	public void ADCI() {
 		//lista com a serializacao dos dados do excel
 		lista = LeituraFicheiro.getDados();
@@ -63,6 +106,10 @@ public class RegrasDetencao {
 				System.out.println("ADCI = " + m);
 		}
 	}
+	
+	/**
+	 * metodo para ver o ADII na comparacao iPlasma e PMD com Is_long_method
+	 */
 	public void ADII() {
 		//lista com a serializacao dos dados do excel
 		lista = LeituraFicheiro.getDados();
