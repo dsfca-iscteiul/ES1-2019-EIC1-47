75861adfa26e758c493c7e1426a080d8ec9fb9e5
Kira <Kira@172.17.3.151>
2019-12-13 14:33:53 +0000

    Regrasdetencao com os testes implementados

--- ES1-2019-EIC1-47/src/tests/RegrasDetencaoTest.java
+++ ES1-2019-EIC1-47/src/tests/RegrasDetencaoTest.java
@@ -8,7 +8,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import classes.LogicParser;
+import classes.RegrasDetencao;
+import classes.RuleChecker;
+
 class RegrasDetencaoTest {
+	private RuleChecker rc = new RuleChecker();
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -28,37 +33,62 @@ class RegrasDetencaoTest {
 
 	@Test
 	final void testVerificacao() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		rd.Verificacao();
+	
+		
 	}
 
 	@Test
 	final void testVerificacaoIntIntIntInt() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		try {
+			rd.Verificacao(2,3,4,6);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+		//fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	final void testVerificacaoIntIntIntIntLogicParser() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		LogicParser lp;
+		try {
+			lp = new LogicParser(0);
+			rd.Verificacao(2,3,4,6,lp);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		//fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	final void testDCI() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		rd.DCI();
+		//fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	final void testDII() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		rd.DII();
 	}
 
 	@Test
 	final void testADCI() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		rd.ADCI();
 	}
 
 	@Test
 	final void testADII() {
-		fail("Not yet implemented"); // TODO
+		RegrasDetencao rd= new RegrasDetencao();
+		rd.ADII();
 	}
 
 }
